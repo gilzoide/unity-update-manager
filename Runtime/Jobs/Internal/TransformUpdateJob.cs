5e256688558ad71e0127bb773fdf3937ffa37c42
gilzoide <gilzoide@gmail.com>
2023-02-13 20:23:11 -0300

    Use an internal unsafe list implementation instead of NativeArray
    This makes it possible to use NativeArray and other data structures from
    managed jobs.

--- Runtime/Jobs/Internal/TransformUpdateJob.cs
+++ Runtime/Jobs/Internal/TransformUpdateJob.cs
@@ -1,5 +1,3 @@
-using Unity.Collections;
-using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine.Jobs;
 
 namespace Gilzoide.UpdateManager.Jobs.Internal
@@ -7,12 +5,11 @@ namespace Gilzoide.UpdateManager.Jobs.Internal
     public struct UpdateTransformJob<TData> : IJobParallelForTransform
         where TData : struct, IUpdateTransformJob
     {
-        public NativeArray<TData> Data;
+        public UnsafeNativeList<TData> Data;
 
         public unsafe void Execute(int index, TransformAccess transform)
         {
-            void* dataPointer = NativeArrayUnsafeUtility.GetUnsafePtr(Data);
-            UnsafeUtility.ArrayElementAsRef<TData>(dataPointer, index).Execute(transform);
+            Data.ItemRefAt(index).Execute(transform);
         }
     }
 }
