8656efbf98d13e2187984745975c00d31df09f60
gilzoide <gilzoide@gmail.com>
2023-02-17 19:50:36 -0300

    Make transform jobs be Burst compilable
    For Burst to actually recognize the job type as compilable, we need to
    explicitly use the concrete `BurstUpdateTransformJob<>` type

---
+++ Runtime/Jobs/Burst/BurstUpdateTransformJob.cs
@@ -0,0 +1,20 @@
+#if HAVE_BURST
+using Gilzoide.UpdateManager.Jobs.Internal;
+using Unity.Burst;
+using UnityEngine.Jobs;
+
+namespace Gilzoide.UpdateManager.Jobs.Burst
+{
+    [BurstCompile]
+    public struct BurstUpdateTransformJob<TData> : IInternalUpdateTransformJob<TData>
+        where TData : struct, IUpdateTransformJob
+    {
+        public UnsafeNativeList<TData> Data { get; set; }
+
+        public unsafe void Execute(int index, TransformAccess transform)
+        {
+            Data.ItemRefAt(index).Execute(transform);
+        }
+    }
+}
+#endif
--- Runtime/Jobs/ITransformJobUpdatable.cs
+++ Runtime/Jobs/ITransformJobUpdatable.cs
@@ -2,29 +2,38 @@ using Gilzoide.UpdateManager.Jobs.Internal;
 
 namespace Gilzoide.UpdateManager.Jobs
 {
-    public interface ITransformJobUpdatable<TData> : IInitialTransformJobDataProvider<TData>
+    public interface ITransformJobUpdatable<TData, TJob> : IInitialTransformJobDataProvider<TData>
+        where TData : struct, IUpdateTransformJob
+        where TJob : struct, IInternalUpdateTransformJob<TData>
+    {
+    }
+
+    public interface ITransformJobUpdatable<TData> : ITransformJobUpdatable<TData, UpdateTransformJob<TData>>
         where TData : struct, IUpdateTransformJob
     {
     }
 
     public static class ITransformJobUpdatableExtensions
     {
-        public static void RegisterInManager<TData>(this ITransformJobUpdatable<TData> updatable)
+        public static void RegisterInManager<TData, TJob>(this ITransformJobUpdatable<TData, TJob> updatable)
             where TData : struct, IUpdateTransformJob
+            where TJob : struct, IInternalUpdateTransformJob<TData>
         {
-            UpdateTransformJobManager<TData>.Instance.Register(updatable);
+            UpdateTransformJobManager<TData, TJob>.Instance.Register(updatable);
         }
 
-        public static void UnregisterInManager<TData>(this ITransformJobUpdatable<TData> updatable)
+        public static void UnregisterInManager<TData, TJob>(this ITransformJobUpdatable<TData, TJob> updatable)
             where TData : struct, IUpdateTransformJob
+            where TJob : struct, IInternalUpdateTransformJob<TData>
         {
-            UpdateTransformJobManager<TData>.Instance.Unregister(updatable);
+            UpdateTransformJobManager<TData, TJob>.Instance.Unregister(updatable);
         }
 
-        public static TData GetJobData<TData>(this ITransformJobUpdatable<TData> updatable)
+        public static TData GetJobData<TData, TJob>(this ITransformJobUpdatable<TData, TJob> updatable)
             where TData : struct, IUpdateTransformJob
+            where TJob : struct, IInternalUpdateTransformJob<TData>
         {
-            return UpdateTransformJobManager<TData>.Instance.GetData(updatable);
+            return UpdateTransformJobManager<TData, TJob>.Instance.GetData(updatable);
         }
     }
 }
