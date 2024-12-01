b437ad820d942b964c30f51e276bd7ca25bb29d3
gilzoide <gilzoide@gmail.com>
2024-12-01 08:23:20 -0300

    Separate JobifiedUpdate from JobifiedUpdateParallel

--- Tests/Performance/ComparePerformance.cs
+++ Tests/Performance/ComparePerformance.cs
@@ -23,26 +23,38 @@ namespace Gilzoide.UpdateManager.Tests.Performance
         [UnityTest, Performance]
         public IEnumerator JobifiedUpdate()
         {
+            yield return RunTest<RotateJobified>(useRootTransform: true);
+        }
+        
+        [UnityTest, Performance]
+        public IEnumerator JobifiedUpdateParallel()
+        {
             yield return RunTest<RotateJobified>();
         }
 
-        public IEnumerator RunTest<T>() where T : MonoBehaviour
+        public IEnumerator RunTest<T>(bool useRootTransform = false) where T : MonoBehaviour
         {
+            Transform rootTransform = useRootTransform ? new GameObject("root").transform : null;
             var objects = new List<GameObject>();
             for (int i = 0; i < 1000; i++)
             {
-                objects.Add(new GameObject(typeof(T).Name + " " + i, typeof(T)));
+                GameObject gameObject = new GameObject(typeof(T).Name + " " + i, typeof(T));
+                gameObject.transform.SetParent(rootTransform);
+                objects.Add(gameObject);
             }
             QualitySettings.vSyncCount = 0;
             Application.targetFrameRate = -1;
             yield return Measure.Frames()
-                // .WarmupCount(100)
                 .MeasurementCount(1000)
                 .Run();
             
             foreach (GameObject obj in objects)
             {
                 Object.Destroy(obj);
+            }
+            if (rootTransform)
+            {
+                Object.Destroy(rootTransform.gameObject);
             }
             yield return null;
         }
